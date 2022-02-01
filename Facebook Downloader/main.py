d2d0361f4fe3a6cc764fee9d6667d0a72ecbd3aa
XxArcaiCxX <peuporsche@hotmail.com>
2022-02-01 01:13:41 +0000

    Add files via upload

---
+++ Facebook Downloader/main.py
@@ -0,0 +1,65 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def download_from_group(url):
+    # Get HTML
+    driver = webdriver.Chrome('C:\\Users\\ArcaiC\\Documents\\chromedriver.exe')
+    driver.get(url)
+    if driver.find_element_by_xpath("//button[@class='_54k8 _52jh _56bs _9y0r _56bt']") is not None:
+        elem = driver.find_element_by_xpath("//button[@class='_54k8 _52jh _56bs _9y0r _56bt']")
+        elem.click()
+    elem = driver.find_element_by_xpath("//div[@class='_1o0y']")
+    elem.click()
+    elem = driver.find_element_by_xpath("//video")
+    video_url = elem.get_attribute('src')
+    driver.close()
+    # Get source Link
+
+    return video_url
+
+
+def download_from_page(url):
+    # Get HTML
+    rawdata = requests.get(url)
+    html = rawdata.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Get source Link
+    div = soup.find("head").find(property="og:video:url")
+    video_url = div['content']
+    return video_url
+
+
+def downloader():
+
+    # Get URL
+    url = sys.argv[1]
+
+    # Convert to mobile
+    url = url.replace("https://www", "https://m")
+    print("URL: " + url)
+
+    video_url = ""
+
+    if url.startswith("https://m.facebook.com/watch/"):
+        print("Video from page")
+        video_url = download_from_page(url)
+    elif url.startswith("https://m.facebook.com/groups"):
+        print("Video from a group")
+        video_url = download_from_group(url)
+
+    # Download video
+    response = requests.get(video_url)
+    print("Downloading")
+    f = open(str(abs(hash(response.url))) + ".mp4", 'wb')
+    for byte in response.iter_content(chunk_size=255):
+        if byte:  # filter out keep-alive chunks
+            f.write(byte)
+    print("Done")
+
+if __name__ == '__main__':
+    downloader()
+
