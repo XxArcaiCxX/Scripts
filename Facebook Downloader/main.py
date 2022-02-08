ca9b004af7dc94b7630f3df8ae95c3245c40b068
XxArcaiCxX <peuporsche@hotmail.com>
2022-02-08 23:06:01 +0000

    Fixed Page downloading

--- Facebook Downloader/main.py
+++ Facebook Downloader/main.py
@@ -24,6 +24,22 @@ def download_from_group(url):
 
 def download_from_page(url):
     # Get HTML
+    driver = webdriver.Chrome('C:\\Users\\ArcaiC\\Documents\\chromedriver.exe')
+    driver.get(url)
+    if driver.find_element_by_xpath("//button[@class='_54k8 _52jh _56bs _9y0r _56bt']") is not None:
+        elem = driver.find_element_by_xpath("//button[@class='_54k8 _52jh _56bs _9y0r _56bt']")
+        elem.click()
+    elem = driver.find_element_by_xpath("//div[@class='_1o0y']")
+    elem.click()
+    elem = driver.find_element_by_xpath("//video")
+    video_url = elem.get_attribute('src')
+    driver.close()
+    # Get source Link
+
+    return video_url
+
+def download_from_watch(url):
+    # Get HTML
     rawdata = requests.get(url)
     html = rawdata.content
     soup = BeautifulSoup(html, 'html.parser')
@@ -46,11 +62,14 @@ def downloader():
     video_url = ""
 
     if url.startswith("https://m.facebook.com/watch/"):
-        print("Video from page")
-        video_url = download_from_page(url)
+        print("Video from watch")
+        video_url = download_from_watch(url)
     elif url.startswith("https://m.facebook.com/groups"):
         print("Video from a group")
         video_url = download_from_group(url)
+    else:
+        print("Video from a page")
+        video_url = download_from_page(url)
 
     # Download video
     response = requests.get(video_url)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Scripts
 
 In here you can find random scripts made by yours truly, click on the directories to know more about a specific script or download them on releases 
+
+## Caution 	⚠️
+The scripts in here are made so that it *works on my machine* ™.
